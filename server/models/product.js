a06f0a0976f16cd677755344a03f822c3c975fc5
Heidi <hlreidwins@gmail.com>
2018-06-18 14:40:45 -0700

    Update

--- server/models/product.js
+++ server/models/product.js
@@ -3,9 +3,10 @@ const mongoose = require('../config/mongoose.js');
 const Schema = mongoose;
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    qty: Number,
-    price: Number
+    name: {type: String, required: [true, 'name required'],minlength: [3, 'product name cannot be less than 3 characters'] },
+    qty: {type: Number, required: [true,'quantity required'],min: [0, 'quantity cannot be less than 0'] },
+    price: {type: Number, required: [true, 'price required'],min: [0 , 'price cannot be less than 0']}
 })
+
 const Product = mongoose.model('Product', ProductSchema)
 module.exports = Product;
